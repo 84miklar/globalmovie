bd28ad4e0bc1599f00f0f823650a3892a329c8cd
Mikael Larsson <mikael_larsson84@hotmail.com>
2021-09-22 11:33:52 +0200

    fixed about css

--- src/views/aboutView/AboutView.js
+++ src/views/aboutView/AboutView.js
@@ -6,33 +6,35 @@ export const AboutView = () => {
   const tmdb = "https://www.themoviedb.org/";
   return (
     <div className="about__container">
-      <Card>
-        <section className="paragraph__container">
-          <p>
-            Global movie is a movie search site made as a school assignment.{" "}
-          </p>
-          <p>
-            The techniques used are JSX and CSS in the React framework and the
-            API used to fetch all the movie data comes from{" "}
-            <a
-              className="tmdb__link"
-              target="_blank"
-              rel="noreferrer"
-              href={tmdb}
-            >
-              the movie data base
+      <section className="card__wrapper">
+        <Card>
+          <section className="paragraph__container">
+            <p>
+              Global movie is a movie search site made as a school assignment.{" "}
+            </p>
+            <p>
+              The techniques used are JSX and CSS in the React framework and the
+              API used to fetch all the movie data comes from{" "}
+              <a
+                className="tmdb__link"
+                target="_blank"
+                rel="noreferrer"
+                href={tmdb}
+              >
+                the movie data base
+              </a>
+            </p>
+
+            <a className="mail__to" href="mailto:mikael_larsson84@hotmail.com">
+              <Button label="Contact me" />
             </a>
-          </p>
 
-          <a className="mail__to" href="mailto:mikael_larsson84@hotmail.com">
-            <Button label="Contact me" />
-          </a>
-
-          <p className="copyright">
-            Ⓒ Copyright by Mikael Larsson, Sweden, 2021
-          </p>
-        </section>
-      </Card>
+            <p className="copyright">
+              Ⓒ Copyright by Mikael Larsson, Sweden, 2021
+            </p>
+          </section>
+        </Card>
+      </section>
     </div>
   );
 };
